ccec8906fa168f18eabc8ea677bdd239bc9debd6
TheDuckMan64 <hamsterello@gmail.com>
2025-11-07 18:31:57 +1100

    Refactor

--- src/main/java/theduckman64/skinkeybindmanager/SkinKeybindManager.java
+++ src/main/java/theduckman64/skinkeybindmanager/SkinKeybindManager.java
@@ -19,6 +19,6 @@ public class SkinKeybindManager implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Client only mod!");
 	}
 }
